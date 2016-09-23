3e1be2ffbd521c57b2cadb96a563f5a0dbcbcab5
Mark1Z <muslim1992@gmail.com>
2016-09-23 15:00:15 +0300

    favicon add

--- wp-content/themes/iloveu/footer.php
+++ wp-content/themes/iloveu/footer.php
@@ -3,6 +3,11 @@
  * footer.php template
  */
 ?>
+<div class="navigation">
+    <?php previous_posts_link('Назад'); ?>
+    <?php next_posts_link('Вперед'); ?>
+</div>
+</main>
 <footer class="footer">
     <div class="container">
         <h1>Я <span class="heart"></span> тебя!</h1>
--- wp-content/themes/iloveu/header.php
+++ wp-content/themes/iloveu/header.php
@@ -11,6 +11,7 @@
 <!--[if (gte IE 9)|!(IE)]><!-->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><!--<![endif]-->
 <head>
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicon.ico'; ?>">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="language" content="en"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,6 +21,17 @@
 <body>
 <header class="header">
     <div class="container">
-        <h1>Дашенька</h1>
+        <h1>Дарюничка</h1>
     </div>
 </header>
+<main class="main">
+    <?php
+    $pageIndex = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $progress = round(($pageIndex / wp_count_posts('post')->publish) * 100);
+    ?>
+    <div class="progress">
+        <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"
+             aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0" aria-valuemax="100"
+             style="width: <?php echo $progress; ?>%">
+        </div>
+    </div>
