514a0a784dd775258417815dd77ac4ca720a626a
Mark1Z <muslim1992@gmail.com>
2016-10-13 10:49:42 +0300

    post order

--- wp-content/themes/iloveu/index.php
+++ wp-content/themes/iloveu/index.php
@@ -5,15 +5,25 @@
 ?>
 <?php get_header(); ?>
 <div class="container">
-    <?php if (have_posts()) :
-        while (have_posts()) :
-            the_post();
+    <?php
+    $args = [
+        'posts_per_page' => 1,
+        'post_type' => 'post',
+        'orderby' => 'menu_order',
+        'order' => 'ASC',
+    ];
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) :
+            $query->the_post();
             ?>
             <div class="redactor-content">
                 <?php the_content(); ?>
             </div>
             <?php
         endwhile;
+        wp_reset_postdata();
         ?>
         <?php
     endif;
